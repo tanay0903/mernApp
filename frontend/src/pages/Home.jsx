fa4e41bcce49b94595e13a9e755b51f7a2bff497
kumart <tanay.kumar@panasonic.aero>
2025-07-14 07:18:53 +0530

    navbar, header, home page , naviagate to login page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+import Header from '../components/Header.jsx'
 
 const home = () => {
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-[url(../bg_img.png)] bg-cover bg-center'>
+      <Navbar/>
+      <Header/>
     </div>
   )
 }
